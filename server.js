dab541ead93505e64d153d67e14d9848b27f474a
nazrul23-human <nazrul23-human@gmail.com>
2026-06-26 06:23:23 +0600

    server connect with MongoDB

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// MongoDB CONNECT
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected."))
+    .catch((err) => console.log("MongoDB Error:", err));
+
+// TEST ROUTE
 app.get("/", (req, res) => {
-    res.send(" Nazrul, Server Running 🚀");
+    res.send("Nazrul, Server Running.");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server running non port", PORT);
+    console.log("Server running on port", PORT);
 });
